7305bb84b2555d713a05f659b327b70ee0d9403e
jeep-odoo <jeep@odoo.com>
2024-02-06 18:29:17 +0530

    wip

---
+++ estate/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "jeep-odoo",
+    'category': 'estate',
+    'application': True,
+    'installable': True,
+}
