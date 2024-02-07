8431ff960f282ab1eb62ec09400dfed8869b99fe
dere-odoo <dere@odoo.com>
2024-02-07 17:12:47 +0530

    [WIP] 1-4 chapters

---
+++ real estate/__manifest__.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+#Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Real Estate',
+    'version' : '1.0',
+    'description': 'A comprehensive real estate management system that allows management of various real estate properties',
+    'summary': 'Real estate',
+    'installable': True,
+    'application': True,
+    'license': 'OEEL-1',
+    'version': '1.0',
+}
---
+++ real estate/models/estate_property.py
@@ -0,0 +1,19 @@
+from odoo import models, fields
+
+class estate_property(models.Model):
+    _name = 'estate.property'
+    _description = 'estate property table for real estate'
+
+    name = fields.Char('name',default = "Property name",required=True)
+    description = fields.Text('description',default='some desc')
+    postcode = fields.Char('postcode',default="ABCD")
+    date_availability = fields.Date('date_availability')
+    expected_price = fields.Float('expected_price',default=0.0,required=True)
+    selling_price = fields.Float('selling_price',default=0.0)
+    bedrooms = fields.Integer('bedrooms',default=0)
+    living_area = fields.Integer('living_area',default=0)
+    facades = fields.Integer('facades',default=0)
+    garage = fields.Boolean('garage',default=False)
+    garden = fields.Boolean('garden',default=False)
+    garden_area = fields.Integer('garden_area',default=0)
+    garden_orientation = fields.Selection([('n','North'),('s','South'),('w','West'),('e','East')])
