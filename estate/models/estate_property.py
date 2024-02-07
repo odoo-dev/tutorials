6b823257735206a5b4ea15311a54584134800a5c
jeep-odoo <jeep@odoo.com>
2024-02-07 18:17:55 +0530

    [ADD] estate: added actions and menus
    Completed the ch-6 implementation. Added actions, views and menus.
    The module was lacking a UI. It had just a DB model. Brought the estate app to
    the main odoo dashboard.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 class estateProperty(models.Model):
     _name = "estate.property"
@@ -7,16 +8,24 @@ class estateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode= fields.Char()
-    date_availability=fields.Date()
+    date_availability=fields.Date(copy=False, default=fields.Date.today()+relativedelta(months=3))
     expected_price=fields.Float(required=True)
-    selling_price=fields.Float()
-    bedrooms=fields.Integer()
-    living_area=fields.Integer
+    selling_price=fields.Float(readonly=True, copy=False)
+    bedrooms=fields.Integer(default=2)
+    living_area=fields.Integer()
     facades=fields.Integer()
+    active = fields.Boolean(default=True)
     garage=fields.Boolean()
     garden=fields.Boolean()
+    state = fields.Selection(
+        string='State',
+        selection=[('new', 'New'), ('or', 'Offer Recieved'), ('oa', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
+        required=True,
+        copy=False,
+        default='new'
+    )
     garden_area=fields.Integer()
     garden_orientation=fields.Selection(
-        string='Type',
+        string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
     )
