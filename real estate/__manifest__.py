c124ee497d9e378cdf71cf65aa24525d4d0782bd
dere-odoo <dere@odoo.com>
2024-02-13 10:27:43 +0530

    [WIP] 5th chapter

--- real estate/__manifest__.py
+++ real estate/__manifest__.py
@@ -2,12 +2,16 @@
 
 {
     'name': 'Real Estate',
-    'version' : '1.0',
+    'version': '1.0',
+    'category': 'Industry/real_estate',
     'description': 'A comprehensive real estate management system that allows management of various real estate properties',
     'summary': 'Real estate',
     'installable': True,
     'application': True,
     'license': 'OEEL-1',
-    'version': '1.0',
-    'depends': ['base']
+    'depends': ['base'],
+    'data':[
+        'security/groups.xml',
+        'ir.model.access.csv'
+        ]
 }
--- real estate/models/estate_property.py
+++ real estate/models/estate_property.py
@@ -3,19 +3,19 @@
 from odoo import models, fields
 
 class estate_property(models.Model):
-    _name = 'estate.property'
-    _description = 'estate property table for real estate'
+    _name = "estate.property"
+    _description = "estate property table for real estate"
 
-    name = fields.Char(string='Name', default="Property name", required=True)
-    description = fields.Text(string='Description', default='some desc')
-    postcode = fields.Char(string='Postcode', default="ABCD")
-    date_availability = fields.Date(string='Date Availability')
-    expected_price = fields.Float(string='Expected Price', default=0.0, required=True)
-    selling_price = fields.Float(string='Selling Price', defaultcd=0.0)
-    bedrooms = fields.Integer(string='Bedrooms', default=0)
-    living_area = fields.Integer(string='Living Area', default=0)
-    facades = fields.Integer(string='Facades', default=0)
-    garage = fields.Boolean(string='Garage', default=False)
-    garden = fields.Boolean(string='Garden', default=False)
-    garden_area = fields.Integer(string='Garden Area', default=0)
-    garden_orientation = fields.Selection(string="Garden Orientation", selection=[('n', 'North'),('s', 'South'),('w', 'West'),('e', 'East')])
+    name = fields.Char(string="Name", default="Property name", required=True)
+    description = fields.Text(string="Description", default="some desc")
+    postcode = fields.Char(string="Postcode", default="ABCD")
+    date_availability = fields.Date(string="Date Availability")
+    expected_price = fields.Float(string="Expected Price", default=0.0, required=True)
+    selling_price = fields.Float(string="Selling Price", defaultcd=0.0)
+    bedrooms = fields.Integer(string="Bedrooms", default=0)
+    living_area = fields.Integer(string="Living Area", default=0)
+    facades = fields.Integer(string="Facades", default=0)
+    garage = fields.Boolean(string="Garage", default=False)
+    garden = fields.Boolean(string="Garden", default=False)
+    garden_area = fields.Integer(string="Garden Area", default=0)
+    garden_orientation = fields.Selection(string="Garden Orientation", selection=[("n", "North"),("s", "South"),("w", "West"),("e", "East")])
