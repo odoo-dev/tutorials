9797098cb0c2cb942eb379f610f2e34deb4648c1
shrm-odoo <shrm@odoo.com>
2024-02-14 11:54:09 +0530

    [ADD] estate: added computed, inverse and onchange methods to models.
    Added computed methods to the estate_property model for calculating garden area
    and best offer, also added onchange method for garden field.
    Also, implemented computed and inverse methods in estate_property_offer model
    for calculating validity and deadline.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -13,17 +13,42 @@ class EstateProperty(models.Model):
     expected_price = fields.Float(string="Expected Price", required=True)
     selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
     bedrooms = fields.Integer(string = "Bedrooms", default="2")
-    living_area = fields.Integer(string = "Living Area (in square meters)")
+    living_area = fields.Integer(string = "Living Area (in sqm)")
     facades = fields.Integer(string="Facades")
     garage = fields.Boolean(string="Garage ?")
-    garden = fields.Boolean(string="Garden ?")
-    garden_area = fields.Integer(string = "Garden Area (in square meters)")
+    garden = fields.Boolean(string="Garden ?", )
+    garden_area = fields.Integer(string = "Garden Area (in sqm)")
     garden_orientation = fields.Selection([('north','North'),('south','South'),('east','East'),('west','West')] , string="Garden Orientation")
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     sales_person_id = fields.Many2one("res.users", string="Salesman", default=lambda self:self.env.user)
     tag_ids = fields.Many2many("estate.property.tags")
     offer_ids = fields.One2many("estate.property.offer", "property_id")
+    total_area = fields.Integer(string="Total Area(in sqm)", compute="_compute_total_area")
+    best_offer = fields.Float(string="Best Offer", compute="_compute_best_offer")
 
     active = fields.Boolean(default=True)
     state = fields.Selection([('new', 'New'),('offer_received', 'Offer received'),('offer_accepted', 'Offer Accepted'),('sold', 'Sold'),('cancelled', 'Cancelled')], string='Status', default='new', copy=False, required=True)
+
+
+    #computed methods
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+    
+    @api.depends("offer_ids")
+    def _compute_best_offer(self):
+        for record in self:
+            best_offer = max(record.offer_ids.mapped("price"), default=0.0)
+            record.best_offer = best_offer
+    
+    #onchange method
+    @api.onchange("garden")
+    def _onchange_garden(self):
+            if self.garden:
+                self.garden_area = 10 
+                self.garden_orientation = 'north'
+            else:
+                self.garden_area = 0
+                self.garden_orientation = False
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from datetime import datetime, timedelta
 
 class EstatePropertyTags(models.Model):
     _name = "estate.property.offer"
@@ -8,3 +9,19 @@ class EstatePropertyTags(models.Model):
     status = fields.Selection([('accepted', 'Accepted'),('refused', 'Refused')], string='Status',copy=False)
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
+    validity = fields.Integer(string="Validity(days)", default=7)
+    date_deadline = fields.Date(string="Deadline", compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    #compute method
+    @api.depends("create_date","validity")
+    def _compute_date_deadline(self):
+        for offer in self:
+            if offer.create_date:
+                offer.date_deadline = offer.create_date + timedelta(days=offer.validity)
+            else:
+                offer.date_deadline = datetime.now() + timedelta(days=offer.validity)
+
+    #inverse method
+    def _inverse_date_deadline(self):
+        for offer in self:
+            offer.validity = (offer.date_deadline - (offer.create_date).date()).days
