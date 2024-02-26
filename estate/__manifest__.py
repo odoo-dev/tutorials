6ec27ab39d482bc2e0d283074200dcab8efa922b
jeep-odoo <jeep@odoo.com>
2024-02-26 16:30:54 +0530

    [ADD] estate: added qweb reports to the module
    Added qweb reports functionality wherein the user can now create and download
    report of an individual property along with the offers if any. Also implemented
    the report inheritance by inheriting the report from the estate module to the
    estate_account module.  One can also now download report for a specific salesman
    Few changes to demo data to avoid errors encountered while installing the module
    in the same db.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -11,6 +11,8 @@
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/inherited_res_user_views.xml',
+        'report/estate_property_templates.xml',
+        'report/estate_property_reports.xml',
         'views/estate_menus.xml',
     ],
     'demo':[
--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -1,9 +1,12 @@
 {
-    'name': "Estate Account",
-    'version': '1.0',
-    'depends': ['estate', 'account'],
-    'author': "jeep-odoo",
-    'category': 'Estate/Estate Account',
-    'installable': True,
-    "license" : "LGPL-3",
+    "name": "Estate Account",
+    "version": "1.0",
+    "depends": ["estate", "account"],
+    "author": "jeep-odoo",
+    "category": "Estate/Estate Account",
+    "data": [
+        "report/estate_account_templates.xml",
+    ],
+    "installable": True,
+    "license": "LGPL-3",
 }
