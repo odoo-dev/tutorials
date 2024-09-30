871ca56942bdb76b1394249269ae61cfb79b81db
Cyril Moreau (cymo) <210376@umons.ac.be>
2024-09-30 19:03:57 +0200

    [IMP] awesome_clicker: Create an action to display more information of the game

--- awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -1,18 +1,30 @@
-/** @odoo-module */
+/** @odoo-module **/
 
 import {registry} from "@web/core/registry";
 import {Component, useExternalListener, useState} from "@odoo/owl";
+import {useService} from "@web/core/utils/hooks";
 
 export class ClickerSystray extends Component {
     static template = "awesome_clicker.ClickerSystray";
 
     setup() {
         this.state = useState({count: 0});
+        this.action = useService("action");
         useExternalListener(document.body, "click", () => this.state.count++, true);
+
     }
 
     increment() {
         this.state.count += 9;
+    }
+
+    openClickAction() {
+        this.action.doAction({
+            type: "ir.actions.client",
+            tag: "awesome_clicker.client_action",
+            target: "new",
+            name: "Clicker"
+        });
     }
 }
 
---
+++ awesome_clicker/static/src/client_action/client_action.js
@@ -0,0 +1,12 @@
+/** @odoo-module **/
+
+import {registry} from "@web/core/registry";
+import {Component} from "@odoo/owl";
+
+export class ClientAction extends Component {
+    static template = "awesome_clicker.ClientAction";
+
+}
+
+registry.category("actions").add("awesome_clicker.client_action", ClientAction);
+
