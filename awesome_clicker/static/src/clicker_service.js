9b244476b7b8bb4b1ec514d3d635b18a66559b0b
Cyril Moreau (cymo) <210376@umons.ac.be>
2024-10-01 11:45:33 +0200

    [IMP] awesome_clicker: Buy clickBots with customizable params

--- awesome_clicker/static/src/clicker_service.js
+++ awesome_clicker/static/src/clicker_service.js
@@ -6,19 +6,46 @@ import {reactive} from "@odoo/owl";
 
 export const clickerService = {
     start(env) {
-        const state = reactive({counter: 0});
+        const state = reactive({
+            counter: 0,
+            level: 0,
+            clickBots: 0,
+        });
+
+        const params = {
+            interval: 10000,
+            clickPerBot: 10,
+        }
 
         document.addEventListener("click", () => {
-            state.counter++;
+            increment(1);
         });
+
+        setInterval(() => {
+            increment(state.clickBots * params.clickPerBot);
+        }, params.interval);
 
         function increment(inc) {
             state.counter += inc;
+            if (state.level < 1 && state.counter >= 1000) {
+                state.level++;
+            }
+        }
+
+        function buyBot() {
+            const clickBotPrice = 1000;
+            if (state.counter < clickBotPrice) {
+                return false;
+            }
+            state.counter -= clickBotPrice;
+            state.clickBots++;
         }
 
         return {
             state,
+            params,
             increment,
+            buyBot,
         };
     },
 };
