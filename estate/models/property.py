9cc2dab56ffd8e05acacdf717976d25a3b576c1f
Mathilde Pascal <mepl@odoo.com>
2024-10-21 16:31:15 +0200

    Up to Chapter 6

--- estate/models/property.py
+++ estate/models/property.py
@@ -3,21 +3,21 @@ from odoo import models, fields
 
 
 class TestModel(models.Model):
-    _name = "estate.property"
-    _description = "Property"
+    _name = 'estate.property'
+    _description = 'Property'
 
-    name = fields.Char(required=True)
+    name = fields.Char(string='Title', required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=lambda self: fields.Datetime.today() + relativedelta(months=3))
+    date_availability = fields.Date(string='Available From', copy=False, default=lambda self: fields.Datetime.today() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string='Living Area (m²)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string='Garden Area (m²)')
     garden_orientation = fields.Selection([
         ('north', 'North'),
         ('east', 'East'),
