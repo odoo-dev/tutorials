0be4c8a51bf67394fcc7812b684f5ff1925ae1e1
Denis Rygaert <dery@odoo.com>
2024-10-21 15:19:53 +0200

    first four chapter of the tutorial

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Real Estate',
+    'summary': 'Track real estate assets',
+    'application': True,
+    'category': 'Tutorials/Estate',
+    'installable': True,
+    'description': "",
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv'
+    ],
+    'license': 'AGPL-3'
+}
