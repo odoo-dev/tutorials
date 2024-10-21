8134c8b46ab191c44d2400363ca39a49d62035b7
Lionel Piraux (lipi) <lipi@odoo.com>
2024-10-21 13:58:00 +0200

    [ADD] chap2: __init__.py, __manifest__.py

---
+++ estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Real Estate",
+    'version': '0.1',
+    'depends': ['base'],
+    'author': "Lionel Piraux (lipi)",
+    'description': "A real estate app",
+    'category': 'Tutorials/RealEstate',
+    'application': True,
+    'installable': True,
+    # data files always loaded at installation
+    'data': [
+
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+
+    ],
+    'license': "LGPL-3",
+}
