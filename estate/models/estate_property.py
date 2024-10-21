70b271933684debadaa072d83f6670eabbe4468d
Lionel Piraux (lipi) <lipi@odoo.com>
2024-10-21 15:18:12 +0200

    [ADD] chap3: section common attributes

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,16 +5,19 @@ class Property(models.Model):
     _name = "estate.property"
     _description = "Real Estate property"
 
-    name = fields.Char()
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float()
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
-    garden_orientation = fields.Selection([("N", "North"), ("S", "South"), ("E", "East"), ("W", "West")])
+    name = fields.Char("Name", required=True)
+    description = fields.Text("Description")
+    postcode = fields.Char("Postcode")
+    date_availability = fields.Date("Availability Date")
+    expected_price = fields.Float("Expected Price", required=True)
+    selling_price = fields.Float("Selling Price")
+    bedrooms = fields.Integer("Bedrooms")
+    living_area = fields.Integer("Living Area")
+    facades = fields.Integer("Facades")
+    garage = fields.Boolean("Garage")
+    garden = fields.Boolean("Garden")
+    garden_area = fields.Integer("Garden Area")
+    garden_orientation = fields.Selection(
+        "Garden Orientation",
+        [("N", "North"), ("S", "South"), ("E", "East"), ("W", "West")]
+    )
