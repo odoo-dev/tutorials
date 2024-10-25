f73312b9ac57ef85b02cc832a60501262a3eaf48
ayve-odoo <ayve@odoo.com>
2024-10-25 12:33:48 +0530

    [IMP] estate: fixed coding standard and bugs
    - renamed the file
    - fixed inheritance problems
    - fixed the checks

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -8,7 +8,9 @@
     Second Application
     """,
     # data files always loaded at installation
-    'data': [],
+    'data': [
+        'report/estate_account_property_invoice.xml'
+    ],
     # data files containing optionally loaded demonstration data
     'demo': [],
     'installable': True,
