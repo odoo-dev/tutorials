f400f19be72a94718342c8e04fbf3b0c366f9485
dabo-odoo <dabo@odoo.com>
2024-10-29 10:53:45 +0100

    [FIX] Second try to fix style and lint to get green runbot

--- awesome_dashboard/controllers/controllers.py
+++ awesome_dashboard/controllers/controllers.py
@@ -6,6 +6,7 @@ from odoo import http
 
 logger = logging.getLogger(__name__)
 
+
 class AwesomeDashboard(http.Controller):
     @http.route('/awesome_dashboard/statistics', type='json', auth='user')
     def get_statistics(self):
