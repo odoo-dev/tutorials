330ea277289734ba65b0ed46a52c82da7e258796
Devendra <djal@odoo.com>
2025-02-03 15:07:43 +0530

    [ADD] real_estate: new module for real estate is created

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "Real Estate",
+
+    'summary': """
+        Having the Real Estate Framework"
+    """,
+
+    'description': """
+        Description of Real Estater App"
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+    'category': 'Tutorials',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': [
+    ],
+
+    'data': [],
+    'assets': {
+    },
+    'license': 'AGPL-3'
+}
