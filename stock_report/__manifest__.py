e1e8130ee2ce4633a7102a34655a1b679f8ae6f4
drat-odoo <drat@odoo.com>
2025-02-26 18:16:34 +0530

    [ADD] stock_report: update picking operations report
    In this commit:
    
    -This commit enables users to view the fields: name, origin, state, and scheduled_date on all pages in Picking Operation report(Inventory).
    
    -Task name: H55 - Picking operation PDF report
    -Task link: https://www.odoo.com/odoo/project/18468/tasks/4443267

---
+++ stock_report/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Stock Report',
+    'version': '1.0',
+    'description': 'Updated Picking Operation report (in Inventory)',
+    'depends': ['stock'],
+    'data': [
+        'report/report_picking_operations_views.xml',
+    ],
+    'license': 'LGPL-3',
+}
