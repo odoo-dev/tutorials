a1bdaeefec875bdd88cc8857b7693296952e4a76
Harsh Shah <hash@odoo.com>
2025-02-28 15:06:31 +0530

    [ADD] project_account_link_date: add a link module for date computation

---
+++ project_account_link_date/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Link Project Date with Account",
+    'version': "1.0",
+    'summary': "Allows the computation of account date based on project planning date",
+    'author': "Harsh Shah (hash)",
+    'depends': ["project_account", "sale_management"],
+    'data': [
+
+    ],
+    'installable': True,
+    'license': "LGPL-3"
+}
