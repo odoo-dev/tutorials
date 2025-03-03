2946c57f5f77ffc5655751db9653b5f5b10488b0
pkri-odoo <pkri@odoo.com>
2025-03-03 14:37:36 +0530

    [IMP] sale_order : Added logic for dividing price and removing tag when order line deleted

--- sale_order/models/sale_order_line.py
+++ sale_order/models/sale_order_line.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import _, api, fields, models
+from odoo import _, fields, models
 
 
 class SaleOrderLine(models.Model):
@@ -9,3 +9,28 @@ class SaleOrderLine(models.Model):
 
     divided_price = fields.Float("Divided Price")
     sale_tag_line_id = fields.Many2many('sale.order.line.tag', string='Tag')
+
+    def unlink(self):
+        for sale_line in self:
+            sale_order_data = self.env['sale.order'].search(
+                [('id', '=', sale_line.order_id.id)])
+            tag_name = ''
+            if sale_line.sale_tag_line_id:
+                sale_order_line_wizard = self.env['sale.order.line.wizard'].search(
+                    [('sale_order_line_id', '=', sale_line.id)])
+                divided_sale_order_line = self.env['sale.order.line'].search(
+                    [('id', '=', sale_order_line_wizard.sales_order_wizard_ids.divided_sale_order_line_id)])
+
+                for tag in sale_line.sale_tag_line_id:
+                    tag_name = f"{abs(sale_order_line_wizard.sales_order_wizard_ids.original_price - float(tag.name))}"
+
+                tag = self.env['sale.order.line.tag'].create(
+                    {'name': tag_name})
+                divided_sale_order_line.sale_tag_line_id = [(4, tag.id)]
+
+            else:
+                for line_data in sale_order_data.order_line:
+                    if line_data.sale_tag_line_id:
+                        line_data.sale_tag_line_id = False
+
+        return super().unlink()
