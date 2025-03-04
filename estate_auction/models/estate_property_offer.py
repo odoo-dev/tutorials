20cebc5944b93d54a378190b8dbf5edb4104a71e
ssud-odoo <ssud@odoo.com>
2025-03-04 18:52:49 +0530

    [ADD] estate_auction: added module for enable auction for estate property
    - adds auction sale_type for estate property
    tasks-4622433

---
+++ estate_auction/models/estate_property_offer.py
@@ -0,0 +1,19 @@
+from odoo import api, fields, models
+from odoo.tools import float_compare
+from odoo.exceptions import UserError
+
+class EstatePropertyOffer(models.Model):
+    _inherit = "estate.property.offer"
+    
+    property_sale_type = fields.Selection(related="property_id.sale_type")
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get("property_id") and vals.get("price"):
+                property = self.env["estate.property"].browse(vals["property_id"])
+                expected_price = property.expected_price
+                if float_compare(vals["price"], expected_price, precision_rounding=0.01) < 0:
+                    raise UserError("The offer price cannot be less than the expected price of %.2f" % expected_price)
+
+        return models.Model.create(self, vals_list)
