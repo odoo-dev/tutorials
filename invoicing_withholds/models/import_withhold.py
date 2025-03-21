eb79129f2542fd6c08709275742e7b2fcb32e23b
ssud-odoo <ssud@odoo.com>
2025-03-21 18:50:02 +0530

    [IMP] invoicing_withholds: implemet xls-based withhold import
    - added functionality to import withhold data from xls/xlsx files
    - validates file format and required columns (number, document number, tax id)
    - extracts invoice details, partner, and journal for withhold creation
    - update the withhold creation in chatter log with invoice link
    - added security access for the model

--- invoicing_withholds/models/import_withhold.py
+++ invoicing_withholds/models/import_withhold.py
@@ -9,6 +9,7 @@ from odoo.exceptions import UserError, ValidationError
 class ImportWithhold(models.Model):
     _name = "import.withhold"
     _description = "imports withhold from xls files"
+    _rec_name = "description"
 
     description = fields.Char(string="Description", help="Enter a brief description of the withhold import.")
     tax_file = fields.Binary(string="Tax File", help="Upload the tax file in XLS format,\n please include invoice name and tax id in file.")
@@ -22,95 +23,100 @@ class ImportWithhold(models.Model):
         default='new'
     )
 
-
     @api.constrains('file_name', 'tax_file')
     def _check_file_extension(self):
         for record in self:
             if record.file_name:
                 extension = os.path.splitext(record.file_name)[1].lower()
                 if extension not in ['.xls', '.xlsx']:
-                    raise ValidationError("The uploaded file is not a valid Excel (.xls or .xlsx) file.")
+                    raise ValidationError(_("The uploaded file is not a valid Excel (.xls or .xlsx) file."))
 
 
     def process_withhold(self):
         for record in self:
             if not record.tax_file:
-                raise UserError("No file is uploaded.")
-            file_data = base64.b64decode(record.tax_file)
-            self._process_xls_data(file_data)
+                raise UserError(_("No file is uploaded."))
+            try:
+                file_data = base64.b64decode(record.tax_file)
+                self._process_xls_data(file_data)
+                record.state = 'done'
+            except Exception as e:
+                self.env.cr.rollback()
+                record.with_context(norecompute=True).write({'state': 'error'})
+                self.env.cr.commit()
+                raise UserError(_(f"Error processing file: {e}"))
 
 
     def _process_xls_data(self, file_data):
-        file_stream = io.BytesIO(file_data)
         try:
+            file_stream = io.BytesIO(file_data)
             workbook = xlrd.open_workbook(file_contents=file_stream.read())
-        except xlrd.XLRDError:
-            raise ValidationError("Invalid Excel file format. Please upload a valid .xls file.")
+            withhold_dict = {}
 
-        sheet = workbook.sheet_by_index(0)
-        headers = {sheet.cell_value(0, col_idx).strip(): col_idx for col_idx in range(sheet.ncols)}
-        required_columns = ["Number", "Document Number", "Tax Id"]
-        missing_columns = [col for col in required_columns if col not in headers]
+            for sheet_index in range(workbook.nsheets):
+                sheet = workbook.sheet_by_index(sheet_index)
+                headers = {sheet.cell_value(0, col_idx).strip().lower(): col_idx for col_idx in range(sheet.ncols)}
 
-        if missing_columns:
-            raise ValidationError(f"Missing required columns: {', '.join(missing_columns)}")
+                required_columns = ["number", "document number", "tax id"]
+                missing_columns = [col for col in required_columns if col not in headers]
+                if missing_columns:
+                    raise ValidationError(_(f"Missing required columns: {', '.join(missing_columns)}"))
 
-        withhold_dict = {}
-        for row_idx in range(1, sheet.nrows):
-            row = sheet.row(row_idx)
-            invoice_name = str(row[headers["Number"]].value).strip()
-            document_number = str(row[headers["Document Number"]].value).strip()
+                for row_idx in range(1, sheet.nrows):
+                    row = sheet.row(row_idx)
+                    invoice_name = str(row[headers["number"]].value).strip()
+                    document_number = str(row[headers["document number"]].value).strip()
 
-            try:
-                tax_id = int(float(row[headers["Tax Id"]].value))
-            except ValueError:
-                raise ValidationError(f"Invalid Tax ID at row {row_idx+1}")
+                    try:
+                        tax_id = int(float(row[headers["tax id"]].value))
+                    except ValueError:
+                        raise ValidationError(_(f"Invalid Tax ID at row {row_idx+1}"))
 
-            invoice = self.env['account.move'].search([
-                ('name', '=', invoice_name),
-                ('move_type', 'in', ['out_invoice', 'in_invoice']),
-                ('state', '=', 'posted')
-            ], limit=1)
+                    invoice = self.env['account.move'].search([
+                        ('name', '=', invoice_name),
+                        ('move_type', 'in', ['out_invoice', 'in_invoice']),
+                        ('state', '=', 'posted')
+                    ], limit=1)
 
-            if not invoice:
-                raise ValidationError(f"Invoice not found: {invoice_name}")
+                    if not invoice:
+                        raise ValidationError(_(f"Invoice {invoice_name} does not exists or not in required type and state."))
 
-            partner = invoice.partner_id
-            journal = invoice.journal_id or self.env['account.journal'].search([
-                ('company_id', '=', self.env.company.id),
-                ('type', '=', 'general')
-            ], limit=1)
+                    partner = invoice.partner_id
+                    try:
+                        journal = int(row[headers['journal id']].value) if 'journal id' in headers else self.env['account.journal'].search([
+                            ('company_id', '=', self.env.company.id),
+                            ('l10n_ec_withhold_type', 'in', ['in_withhold', 'out_withhold'])
+                        ], limit=1).id
+                    except ValueError:
+                        raise ValidationError(_(f"Invalid Journal Id at row {row_idx+1}"))
+                    if not journal:
+                        raise ValidationError(_(f"Invoice {invoice_name} does not have any valid journal."))
 
-            invoice_id = invoice.id
-            if invoice_id in withhold_dict:
-                withhold_dict[invoice_id]['tax_ids'].append(tax_id)
-            else:
-                withhold_dict[invoice_id] = {
-                    'invoice_id': invoice,
-                    'partner_id': partner,
-                    'journal_id': journal,
-                    'document_number': document_number,
-                    'tax_ids': [tax_id],
-                }
+                    invoice_id = invoice.id
+                    if invoice_id in withhold_dict:
+                        withhold_dict[invoice_id]['tax_ids'].append(tax_id)
+                    else:
+                        withhold_dict[invoice_id] = {
+                            'invoice_id': invoice,
+                            'partner_id': partner,
+                            'journal_id': journal,
+                            'document_number': document_number,
+                            'tax_ids': [tax_id],
+                        }
 
-        for withhold in withhold_dict.values():
-            self.create_invoice_withhold(withhold)
+            for withhold in withhold_dict.values():
+                self.create_invoice_withhold(withhold)
+
+        except Exception as e:
+            raise ValidationError(_(f"Error processing data: {e}"))
 
 
     def create_invoice_withhold(self, withhold_vals):
         invoice = withhold_vals.get('invoice_id')
-
-        if not invoice:
-            raise UserError(f"Invoice not found: {withhold_vals.get('invoice_id').name}")
-
-        journal_id = self.env['account.journal'].search([
-            ('company_id', '=', self.env.company.id),
-            ('type', '=', 'general')
-            ], limit=1).id
-
+        journal_id = withhold_vals.get('journal_id')
         document_number = withhold_vals.get('document_number')
-        
-        tds_wizard = self.env['l10n_ec.wizard.account.withhold'].with_context(
+
+        wizard = self.env['l10n_ec.wizard.account.withhold'].with_context(
             active_model='account.move', active_ids=invoice.ids
         ).create({
             'journal_id': journal_id,
@@ -125,7 +131,7 @@ class ImportWithhold(models.Model):
             ]
         })
 
-        res = tds_wizard.action_create_and_post_withhold()
+        res = wizard.action_create_and_post_withhold()
 
         if res:
             invoice_link = invoice._get_html_link(title=invoice.name)
