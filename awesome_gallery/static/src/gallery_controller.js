46c5568f2aa24e086fa0b3683245fd35a9c9869b
Serhii Rubanskyi - seru <rubanskiysergiy@gmail.com>
2025-03-28 09:18:41 +0100

    Awesome gallery

---
+++ awesome_gallery/static/src/gallery_controller.js
@@ -0,0 +1,5 @@
+import { Component, xml } from "@odoo/owl";
+
+export class GalleryController extends Component {
+  static template = `awesome_gallery.gallery_controller`;
+}
--- awesome_gallery/static/src/gallery_view.js
+++ awesome_gallery/static/src/gallery_view.js
@@ -1,3 +1,16 @@
 /** @odoo-module */
 
 // TODO: Begin here!
+import {registry} from "@web/core/registry";
+import { GalleryController } from "./gallery_controller";
+
+export const GalleryView={
+    type: "gallery",
+    display_name: "Gallery",
+    icon: "oi oi-view-gallery",
+    multiRecord: true,
+    Controller: GalleryController
+};
+
+registry.category("views").add("gallery", GalleryView);
+console.log("XXXXXXXXXXXXXXXXXXXXX\n", registry.category("views"));
