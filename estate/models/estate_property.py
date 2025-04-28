a3603bfdef18b07bfa12661a0b0b0ce4eba20ef3
Woojang Pyeon <wopy@odoo.com>
2025-04-28 11:23:38 +0800

    [ADD] estate: add calculated field for property's best offer price

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -49,11 +49,18 @@ class EstatePropertyModel(models.Model):
     offer_ids = fields.One2many("estate_property_offer", "property_id", string="Offers")
 
     total_area = fields.Integer(compute="_compute_area")
+    best_price = fields.Float(compute="_compute_best_price")
     
     @api.depends("living_area", "garden_area")
     def _compute_area(self):
         for record in self:
             record.total_area = record.living_area + record.garden_area
+
+    @api.depends("offer_ids")
+    def _compute_best_price(self):
+        for record in self:
+            if record.offer_ids:
+                record.best_price = min(record.offer_ids.mapped("price"))
 
     @api.onchange("garden")
     def _onchange_garden(self):
