1350afa74bca1d13bb0156046153e7637f8ab300
Natasya (naza) <naza@odoo.com>
2025-05-06 15:39:22 +0700

    [ADD] estate_account: models
    This commit introduces the Server framework 101 implementation from chapter 1-15

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate Account",
+
+    'summary': """
+        Estate account summary
+    """,
+
+    'description': """
+        Estate account description
+    """,
+
+    'depends': ['estate', 'account'],
+    'application': True,
+    'installable': True,
+    'data': [
+    ]
+}
