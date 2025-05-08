0c8ce776602c46752e3786854b849287f4ea8627
Woojang Pyeon <wopy@odoo.com>
2025-05-08 12:42:41 +0800

    [ADD] case_study_report: add partners contact report and extend invoice report

---
+++ case_study_report/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': '[Case Study] Report',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'report/invoice_report_template.xml',
+        
+        'report/res_partner_template.xml',
+        'report/res_partner_report.xml',
+    ],
+    'application': False,
+}
