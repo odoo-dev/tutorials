1ebe852f58cda33efb12063cd893186547ec78fe
Woojang Pyeon <wopy@odoo.com>
2025-05-14 17:25:15 +0800

    [ADD] case_study_data_access: create new user group "group_sale_salesman_team_leader"

---
+++ case_study_data_access/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': '[Case Study] Data Access',
+    'category': 'Sales/Sales',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'security/sales_team_security.xml',
+    ],
+    'application': False,
+    'installable': True,
+}
