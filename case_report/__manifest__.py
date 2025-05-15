0ee756f4d6c7cfe0be4165cd5eaabf46b15a16d4
Natasya (naza) <naza@odoo.com>
2025-05-15 11:38:17 +0700

    [ADD] case_report: report
    This commit introduces the Case Study: Reports implementation

---
+++ case_report/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Case Study: Reports',
+    'summary': 'App for case study: reports',
+    'description': 'Sale branch App for case study: reports',
+    'license': 'GPL-3',
+    'depends': ['base', 'account', 'contacts'],
+    'data': [
+        'report/templates.xml',
+        'report/reports.xml',
+    ],
+    'application': True,
+    'installable': True,
+}
