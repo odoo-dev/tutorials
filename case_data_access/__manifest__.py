f74fd263e43a308feb45d0ed712fea0cd1cd1d10
Natasya (naza) <naza@odoo.com>
2025-05-19 10:51:57 +0700

    [ADD] case_data_access: security
    This commit introduces the Case Study: Data Access implementation

---
+++ case_data_access/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Case Study: Data Access',
+    'summary': 'Case study data access implementation',
+    'description': 'Case study data access implementation description',
+    'license': 'GPL-3',
+    'depends': ['sale','sales_team'],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+    ],
+    'application': True,
+    'installable': True,
+}
