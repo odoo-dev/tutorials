6130df3767002a57f204bf318820945bfa29b99e
maedekey <maeva.80@hotmail.com>
2025-05-19 14:15:25 +0200

    [ADD] module: Created special real estate app.
    Simple manifest and empty init for the new real estate app. This change is done to follow the odoo tutorial, supervised by the Great and Magnificent Senna. This guy is awesomely patient, thanks to him. No special technical choices here.
    
    task-None
    Fixes None
    Closes None
    opw None

---
+++ estate/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of MDEK technical training
+
+{
+    'name': 'Real Estate !!',
+    'version': '0.0',
+    'category': 'Estate :3',
+    'summary': 'Manage your real uwustate!!',
+    'description': "Manage your fake estate XD",
+    'website': 'https://www.odoo.com/page/crm',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
