ab2ee952359707e7772a1113282b78f807b5fc0a
Natasya (naza) <naza@odoo.com>
2025-05-19 11:01:47 +0700

    [IMP] case_pos, case_report: checkstyle
    This commit resolves some checkstyle issues:
    - E251: Unexpected spaces around keyword / parameter equals

--- case_pos/models/product.py
+++ case_pos/models/product.py
@@ -8,9 +8,9 @@ class ProductProduct(models.Model):
     def get_product_info_pos(self, price, quantity, pos_config_id):
         product_info = super().get_product_info_pos(price, quantity, pos_config_id)
         product_info.update(
-            weight = self.weight,
-            weight_uom_name = self.weight_uom_name,
+            weight=self.weight,
+            weight_uom_name=self.weight_uom_name,
             volume=self.volume,
-            volume_uom_name = self.volume_uom_name
+            volume_uom_name=self.volume_uom_name
         )
         return product_info
