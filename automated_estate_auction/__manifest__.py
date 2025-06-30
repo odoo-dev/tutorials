c7fc7087486564cd58205e11c56198e5ffd137e9
alap-odoo <alap@odoo.com>
2025-06-30 18:49:26 +0530

    [IMP] automated_estate_auction, estate: added website view and custom filters
    added the properties to the website view and custom filters in the estate and
    automated_estate_auction  module. Moreover, updated the config settings
    of the estate to install the module based on the boolean value. Additionally,
    added the make offer for property through website functionality.

--- automated_estate_auction/__manifest__.py
+++ automated_estate_auction/__manifest__.py
@@ -6,9 +6,14 @@
     "data": [
         "security/ir.model.access.csv",
         "data/estate_property_mail_template.xml",
+        "views/estate_property_offer_placed_views.xml",
+        "views/estate_property_make_offer_views.xml",
+        "views/estate_property_detail_website.xml",
+        "views/estate_property_list_website.xml",
         "views/estate_property_views.xml",
     ],
     "sequence": 1,
     "application": True,
     "license": "OEEL-1",
+    "installable": True,
 }
