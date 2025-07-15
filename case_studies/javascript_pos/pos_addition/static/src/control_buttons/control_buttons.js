b56d11bc94c2e037ee920c50c0ff85f3190a1dad
Daryl Kevin (DKEV) <dkev@odoo.com>
2025-07-15 15:36:38 +0800

    [IMP] javascript_pos: select last product after a product is cleared

--- case_studies/javascript_pos/pos_addition/static/src/control_buttons/control_buttons.js
+++ case_studies/javascript_pos/pos_addition/static/src/control_buttons/control_buttons.js
@@ -4,10 +4,19 @@ import { patch } from "@web/core/utils/patch";
 patch(ControlButtons.prototype, {
 
     removeOrderline() {
-        const selectedOrderline = this.currentOrder.get_selected_orderline()
+        // debugger;
+
+        const currentOrder = this.currentOrder
+        const orderLines = currentOrder.get_orderlines()
+        const selectedOrderline = currentOrder.get_selected_orderline()
 
         if (selectedOrderline) {
             selectedOrderline.delete()
+
+            if (orderLines.length > 0) {
+                const lastOrderLine = orderLines[orderLines.length - 1]
+                currentOrder.select_orderline(lastOrderLine)
+            }
         }
     }
 
