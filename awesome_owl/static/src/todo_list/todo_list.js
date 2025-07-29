fd76e40e5f9652c3a21783128b70222d6d9aa994
Jérôme Belpaire <jerome.belpaire@gmail.com>
2025-07-29 17:18:11 +0200

    [ADD] awesome_owl: toggle

--- awesome_owl/static/src/todo_list/todo_list.js
+++ awesome_owl/static/src/todo_list/todo_list.js
@@ -1,4 +1,4 @@
-import { Component, useState } from "@odoo/owl";
+import { Component, onMounted, useRef, useState } from "@odoo/owl";
 import { TodoItem } from "./todo_item";
 
 export class TodoList extends Component {
@@ -7,8 +7,25 @@ export class TodoList extends Component {
 
     setup() {
         this.todos = useState([
-            { id: 2, description: "write tutorial", isCompleted: true },
-            { id: 3, description: "buy milk", isCompleted: false },
+            { id: 1, description: "write tutorial", isCompleted: true },
+            { id: 2, description: "buy milk", isCompleted: false },
         ]);
+        this.todo_counter = 3;
+        this.inputRef = useRef("input_field");
+        onMounted(() => {
+            this.inputRef.el.focus();
+        });
+    }
+
+    addTodo(ev) {
+        if (ev.keyCode == 13 && ev.target.value) {
+            this.todos.push({ id: this.todo_counter++, description: ev.target.value, isCompleted: false });
+            ev.target.value = "";
+        }
+    }
+
+    toggleState(todoId) {
+        const todo = this.todos.find((t) => t.id === todoId);
+        todo.isCompleted = !todo.isCompleted;
     }
 }
