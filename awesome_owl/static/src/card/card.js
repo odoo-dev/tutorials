f48722b54e6fd734f7f95d2baf19372c0bf43e2c
Jérôme Belpaire <jerome.belpaire@gmail.com>
2025-07-30 11:17:35 +0200

    [ADD] awesome_owl: use slots

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,12 +1,21 @@
 /** @odoo-module **/
 
-import { Component, markup } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
 
     static props = {
         title: { type: String },
-        content: { type: String },
+        slots: { type: Object },
     };
+
+    setup() {
+        this.showCard = useState({ value: true });
+    }
+
+    toggleCard() {
+        console.log("toggling");
+        this.showCard.value = !this.showCard.value;
+    }
 }
