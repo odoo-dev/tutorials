b1356449b5cb2c94f81caf7c7c40f0fc0bf81219
Jérôme Belpaire <jerome.belpaire@gmail.com>
2025-07-31 09:58:38 +0200

    [ADD] awesome_dashboard: layout base

--- awesome_dashboard/__manifest__.py
+++ awesome_dashboard/__manifest__.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Awesome Dashboard",
-
     'summary': """
         Starting module for "Discover the JS framework, chapter 2: Build a dashboard"
     """,
-
     'description': """
         Starting module for "Discover the JS framework, chapter 2: Build a dashboard"
     """,
-
     'author': "Odoo",
     'website': "https://www.odoo.com/",
     'category': 'Tutorials/AwesomeDashboard',
@@ -17,7 +14,6 @@
     'application': True,
     'installable': True,
     'depends': ['base', 'web', 'mail', 'crm'],
-
     'data': [
         'views/views.xml',
     ],
@@ -26,5 +22,5 @@
             'awesome_dashboard/static/src/**/*',
         ],
     },
-    'license': 'AGPL-3'
+    'license': 'AGPL-3',
 }
