b8f9458823869b41d48c999a826d09f057e308ff
Arnav Varshney <avar@odoo.com>
2025-08-04 15:33:53 +0800

    [ADD] estate: empty module

---
+++ estate/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'category': 'Sales',
+    'sequence': 1,
+    'summary': 'Tutorial Estate Application',
+    'website': 'https://www.odoo.com/app/estate',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': True,
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
