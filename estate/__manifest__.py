6bf03eaec9c4425e4c7c8b696b3d38ede1f2ab00
Arnav Varshney <avar@odoo.com>
2025-08-04 17:55:50 +0800

    [ADD] estate: add menu items
    Signed-off-by: Arnav Varshney <avar@odoo.com>

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -13,6 +13,9 @@
     ],
     'data': [
         'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'installable': True,
     'application': True,
