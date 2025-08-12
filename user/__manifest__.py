92de997723f2855f31eb7851b84cec5401b113d6
erle <erle@odoo.com>
2025-08-12 11:31:59 +0800

    [ADD] user: user contact report

---
+++ user/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'user',
+    'version': '1.0',
+    'description': 'user report bro',
+    'summary': '',
+    'author': '',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': '',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'report/user_contact_template.xml',
+        'report/user_contact_report.xml',
+    ],
+    'auto_install': False,
+    'application': False,
+}
