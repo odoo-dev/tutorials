7c1c425eb1f141001b4bdc657f416d55f08ee051
jand-odoo <jand@odoo.com>
2025-09-05 12:03:31 +0800

    [IMP[ js 1.4

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,13 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground"; 
     static components = { Counter, Card };
+
+    html1 = "<div class='text-primary'>some contents</div>";
+    html2 = markup("<div class='text-primary'>some contents</div>");
 }
