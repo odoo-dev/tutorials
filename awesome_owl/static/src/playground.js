1d3eea27ef7e3eb4e408cbe605e2c3c6f33ceadc
jand-odoo <jand@odoo.com>
2025-09-05 14:41:04 +0800

    [IMP] js 1.6

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, markup } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
@@ -8,6 +8,13 @@ export class Playground extends Component {
     static template = "awesome_owl.playground"; 
     static components = { Counter, Card };
 
-    html1 = "<div class='text-primary'>some contents</div>";
-    html2 = markup("<div class='text-primary'>some contents</div>");
+    setup() {
+        this.sum = useState({ value: 0 });
+        this.html1 = "<div class='text-primary'>some contents</div>";
+        this.html2 = markup("<div class='text-primary'>some contents</div>");
+    }
+
+    incrementSum() {
+        this.sum.value++;
+    }
 }
