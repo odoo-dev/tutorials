419957be1b3c7b507aa78f00289dbb0a381ac4e4
jand-odoo <jand@odoo.com>
2025-09-08 12:30:34 +0800

    [ADD] Case Study: JS (Product Info)

---
+++ case_study_js/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Case Study: Javascript (PoS)",
+    "category": "Tutorials",
+    "depends": [
+        "base", "point_of_sale"
+    ],
+    "data": [
+    ],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "case_study_js/static/src/**/*"
+        ],
+    },
+    "installable": True,
+}
