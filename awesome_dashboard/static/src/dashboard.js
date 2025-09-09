a504476cb087c227afe01df4843bab1079d83f86
jand-odoo <jand@odoo.com>
2025-09-09 10:11:06 +0800

    [IMP] js 2.6

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -5,11 +5,11 @@ import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { DashboardItem } from "@awesome_dashboard/dashboard_item/dashboard_item";
-import { rpc } from "@web/core/network/rpc";
+import { PieChart } from "@awesome_dashboard/pie_chart/pie_chart";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout, DashboardItem};
+    static components = { Layout, DashboardItem, PieChart };
 
     setup() {
         this.action = useService("action");
