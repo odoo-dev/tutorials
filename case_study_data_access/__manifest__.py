1122b1454dd85bab3046e17acd7ba1196f35baff
jand-odoo <jand@odoo.com>
2025-09-10 10:30:18 +0800

    [Add] Case Study: Data Access

---
+++ case_study_data_access/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Case Study: Data Access",
+    "category": "Tutorials",
+    "depends": [
+        "base", "sale_management", "account"
+    ],
+    "data": [
+        "security/security.xml"
+    ],
+    "installable": True,
+}
