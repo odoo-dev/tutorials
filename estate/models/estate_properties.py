23db79f02000bb47c2e2d48fee8cf9d22dfa05e4
qucol-odoo <qucol@odoo.com>
2025-10-21 14:17:06 +0200

    [IMP] estate: Chapter 7

--- estate/models/estate_properties.py
+++ estate/models/estate_properties.py
@@ -18,7 +18,6 @@ class PropertyModel(models.Model):
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Orientation',
         selection=[
             ('north', 'North'),
             ('south', 'South'),
@@ -29,7 +28,6 @@ class PropertyModel(models.Model):
 
     active = fields.Boolean(default=True)
     state = fields.Selection(
-        string="State",
         selection=[
             ("new", "New"),
             ("received", "Offer Received"),
@@ -41,3 +39,8 @@ class PropertyModel(models.Model):
         copy=False,
         default="new"
     )
+    property_type_id = fields.Many2one("estate.property.type")
+    buyer = fields.Many2one("res.partner", copy=False)
+    salesperson = fields.Many2one("res.users", default=lambda self: self.env.user)
+    tag_ids = fields.Many2many("estate.property.tag")
+    offer_ids = fields.One2many("estate.property.offer", "property_id")
