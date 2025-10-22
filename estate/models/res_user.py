4601f8368244539ae7e1a056a2c126ad456569d9
qucol-odoo <qucol@odoo.com>
2025-10-22 23:38:30 +0200

    [IMP] estate: Chapter 12

---
+++ estate/models/res_user.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+
+class ResUser(models.Model):
+    _inherit = "res.users"
+
+    property_ids = fields.One2many(
+        "estate.property", "salesperson"
+        , domain="['|', ('state', '=', 'new'), ('state', '=', 'received')]"
+    )
