4cac863c3ad0aaf6e6c17d02eb3bd5475492e34d
frtan-odoo <frtan@odoo.com>
2025-12-02 11:52:43 +0700

    [ADD] estate_account: models, security

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,10 @@
+# 19.0-tutorial-frtan
+{
+    'name': "Estate Account",
+    'depends': ['base', 'estate', 'account'],
+    'application': False,
+    'installable': True,
+    'data': [
+        'security/ir.model.access.csv'
+    ]
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,27 @@
+# 19.0-tutorials-frtan
+
+from odoo import Command, models
+
+class EstateProperty(models.Model):
+    _inherit = 'estate.property'
+
+    def action_sold_estate_property(self):
+        for record in self:
+            self.env['account.move'].create([{
+                'partner_id': record.buyer_id.id, # idk why it has to get the id, i thought buyer_id = id
+                'move_type': 'out_invoice',
+                'invoice_line_ids': [
+                    Command.create({ # this does not need brackets [] (expects a dict, not list[dict])
+                        # 'move_id': supposedly this.id but not needed due to Command
+                        'name': record.name,
+                        'quantity': 1,
+                        'price_unit': 0.06 * record.selling_price,
+                    }),
+                    Command.create({
+                        'name': 'Administrative Fees',
+                        'quantity': 1,
+                        'price_unit': 100,
+                    })
+                ]
+            }])
+        return super().action_sold_estate_property()
