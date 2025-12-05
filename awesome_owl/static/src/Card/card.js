a6f6668c13e5b6465151d978254bf4aa42bccd82
frtan-odoo <frtan@odoo.com>
2025-12-05 08:31:56 +0700

    [IMP] owl 3: simple card component

---
+++ awesome_owl/static/src/Card/card.js
@@ -0,0 +1,9 @@
+import { Component, useState } from "@odoo/owl"
+
+export class Card extends Component {
+    static props = {
+        title: {type: String},
+        content: {type: String}
+    };
+    static template = "awesome_owl.card";
+}
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,9 +1,10 @@
 import { Component, useState } from "@odoo/owl";
 import {Counter} from "./Counter/counter";
+import {Card} from "./Card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter };
+    static components = { Counter, Card };
 
     setup(){
         this.state = useState({value: 0});
