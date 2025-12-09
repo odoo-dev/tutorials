ed4700c89a8af22de53760d8605ea454dfcec10c
frtan-odoo <frtan@odoo.com>
2025-12-09 17:19:40 +0700

    [ADD] casestudy_access: config data access

---
+++ casestudy_access/__manifest__.py
@@ -0,0 +1,10 @@
+# case study: data access
+{
+    'name': 'ext_casestudy_access',
+    'depends': ['base', 'sale'],
+    'application': False,
+    'installable': True,
+    'data': [
+        'security/crm_security.xml'
+    ],
+}
