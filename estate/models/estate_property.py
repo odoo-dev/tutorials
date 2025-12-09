428185b0367fcbb32cd2cedfddb4781980deb5c1
frtan-odoo <frtan@odoo.com>
2025-12-09 14:36:20 +0700

    [IMP] estate: multicompany security and visibility

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -61,6 +61,7 @@ class EstateProperty(models.Model):
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
     total_area = fields.Integer(compute="_compute_total_area", string="Total Area (sqm)")
     best_price = fields.Float(compute="_compute_best_price", string="Best Offer")
+    company_id = fields.Many2one("res.company", string="Company", required=True, default=lambda self: self.env.company)
 
     @api.model
     def _read_group_property_type_ids(self, stages, domain):
