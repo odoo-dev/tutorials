25059b8ec409e06e60d62d85f4517c65221c9a8a
Gilles (gicha) <gicha@odoo.com>
2025-12-23 09:34:22 +0100

    [IMP] awesome_clicker: create a client action and move the state to a service.
    The client action will display a dedicated window that will serve to play the game. In the current state, it only contains an increment button with a total value of 10.
    The clicks state has been transfer to a service to share it between the systray item and the client action. That way the clicks count will be the same for the entire web client.
    The service also implement the increment() function to replace the one that was defined in the systray item class.

--- awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -1,16 +1,23 @@
 import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
 
 class ClickerSystray extends Component {
     static template = "awesome_clicker.ClickerSystray";
     static props = {};
 
     setup() {
-        this.state = useState({ counter: 0 });
+        this.clickerService = useState(useService("awesome_clicker.clicker"));
+        this.action = useService("action");
     }
 
-    increment() {
-        this.state.counter++;
+    openClientAction() {
+        this.action.doAction({
+            type: "ir.actions.client",
+            tag: "awesome_clicker.client_action",
+            target: "new",
+            name: "Clicker",
+        });
     }
 }
 
---
+++ awesome_clicker/static/src/client_action/client_action.js
@@ -0,0 +1,14 @@
+import { Component, useState } from "@odoo/owl";
+import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+
+class ClickerClientAction extends Component {
+    static template = "awesome_clicker.ClientAction";
+    static props = ["*"];
+
+    setup() {
+        this.clickerService = useState(useService("awesome_clicker.clicker"));
+    }
+}
+
+registry.category("actions").add("awesome_clicker.client_action", ClickerClientAction);
