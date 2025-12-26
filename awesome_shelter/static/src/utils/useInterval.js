3a084a24c6339ad0b5198e3735885d9fac1b73cd
Gilles (gicha) <gicha@odoo.com>
2025-12-26 08:54:52 +0100

    [IMP] awesome_shelter: add auto-reload on the shelter kanban view.
    This will enable auto-reload on the kanban view of the shelter app every 10 seconds.
    This has been achieved by extending the kanban view, adding an interval an applying this class to the kanban element in the xml of the animals views.

---
+++ awesome_shelter/static/src/utils/useInterval.js
@@ -0,0 +1,8 @@
+import { browser } from "@web/core/browser/browser";
+import { onMounted, onWillUnmount } from "@odoo/owl";
+
+export function useInterval(callback, duration){
+    let interval;
+    onMounted(() => (interval = browser.setInterval(callback, duration)));
+    onWillUnmount(() => (browser.clearInterval(interval)));
+}
