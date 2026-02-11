08c4f76f9d1f0cd6c345121f4018fdc66793e55c
mibav-odoo <mibav@odoo.com>
2026-02-11 17:42:22 +0530

    [ADD] deposit_rental: initialize new module
    - Add manifest file for the new deposit_rental module

---
+++ deposit_rental/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Deposit in rental",
+    "version": "0.1",
+    "summary": "Add feture to deposit in rental app",
+    "description": "This module is use to add functionality in rental app",
+    "depends": ["sale_renting"],
+    "author": "Milan Bavishi",
+    "license": "LGPL-3",
+}
