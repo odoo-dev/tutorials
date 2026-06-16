f9f7fe76f53160a8814b973cd7879e099c31b222
Antoine Chevalier (chean) <chean@odoo.com>
2026-06-16 10:29:14 +0200

    [ADD] estate: complete chapter 7

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 from odoo import fields, models
 from dateutil.relativedelta import relativedelta
 
@@ -9,27 +6,20 @@ class Property(models.Model):
     _name = "estate_property"
     _description = "Estate Property"
 
-    name = fields.Char(
-        "Title",
-        required=True
-    )
+    name = fields.Char("Title", required=True)
     active = fields.Boolean(default=True)
     description = fields.Text()
     postcode = fields.Char("Postcode")
     date_availability = fields.Date(
         "Available From",
-        copy=False, 
-        default=lambda self: fields.Date.today() + relativedelta(months=3)
+        copy=False,
+        default=lambda self: fields.Date.today() + relativedelta(months=3),
     )
     expected_price = fields.Float(
         "Expected Price",
         required=True,
     )
-    selling_price = fields.Float(
-        "Selling Price",
-        readonly=True,
-        copy=False
-    )
+    selling_price = fields.Float("Selling Price", readonly=True, copy=False)
     bedrooms = fields.Integer(
         "Bedrooms",
         default=2,
@@ -40,24 +30,43 @@ class Property(models.Model):
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Garden Orientation',
+        string="Garden Orientation",
         selection=[
-            ('north', 'North'), 
-            ('south', 'South'), 
-            ('east', 'East'), 
-            ('west', 'West'),
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
         ],
     )
     state = fields.Selection(
-        string='State',
+        string="State",
         selection=[
-            ('new', "New"),
-            ('offer_received', "Offer Received"),
-            ('offer_accepted', "Offer Accepted"),
-            ('sold', "Sold"),
-            ('canceled', "Canceled"),
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
         ],
         required=True,
         copy=False,
-        default='new',
+        default="new",
     )
+
+    property_type_id = fields.Many2one(
+        "estate.property.type",
+        string="Property Type",
+    )
+    salesman_id = fields.Many2one(
+        "res.users",
+        string="Salesman",
+        default=lambda self: self.env.user,
+    )
+    buyer_id = fields.Many2one(
+        "res.partner",
+        string="Buyer",
+        copy=False,
+    )
+
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
