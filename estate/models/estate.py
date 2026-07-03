4df195d9d99a38bdc152981c77728b48129cfbbd
Harsh Gavit (hagav) <hagav@odoo.com>
2026-07-03 11:36:18 +0530

    [ADD] estate: chapter 1 to 7 (squash)
    -by following guide of odoo 19.0 docs chapter 2
    
    [IMP] estate: basic model for estate module
    
    [IMP] estate: add ir.model.access.csv file for security of access rights
    
    [FIX] estate: update missing properties in manifest
    
    [LINT] estate:  add new line to fix  type check errors
    
    [IMP] estate: add Pyright and Odoo LS configs
    
    Introduce configuration files for Pyright and the Odoo Language
    Server directly inside the addon to optimize the development workflow. - for zed
    
    [IMP] estate: add missing fields to Estate Model
    
    [IMP] estate: add views(actions) & menus
    
    - action defined in estate_views.xml and linked with esate model
    - three level menus added using nested menuitem element
    - updated manifest to include menus & views files
    
    [IMP] estate: set default values and other field customizations
    
    - set default value in date_availability (3 months from current date)
    - set selling_price to readonly
    - bedrooms have now default value of 2
    
    [IMP] estate: add state(selection) & active(bool) fields
    
    [IMP] estate: add list view
    
    - uses some useful fields from estate model
    
    [IMP] estate: update form ui
    
    - define form_view in views xml
    
    [IMP] estate: add search shortcuts, filters & groupBy
    
    - searchbar now suggests search in different fields when user types.
    - filter using availability based on state
    - groupBy based on Postcode
    - addon PATH correction in odools.toml
    
    [IMP] estate: add missing fields in views
    
    - active & state added to view
    
    [IMP] estate: add Drizzle Studio for local database viewing
    
    - Improves devEx for DB Viewer
    - Isolated tool for local use only, does not affect the estate addon
    
    [IMP] estate: New model estate.type is added to use as type of estate
    
    - linked as Many2one field
    - add access config in access file for new model
    
    [IMP] estate: add buyer & seller to model and view
    
    [IMP] estate: add estate.tag model for tags
    
    - linked with estate using many2many
    
    [IMP] estate: add estate.offer model
    
    - adds offers page (tab) to estate form view
    - linked using one2many field  [property_id (many2one) from
      estate.offer]

---
+++ estate/models/estate.py
@@ -0,0 +1,49 @@
+from dateutil.relativedelta import relativedelta
+from odoo import fields, models
+
+
+class Estate(models.Model):
+    _name = "estate"
+    _description = "Real Estate Module"
+
+    # fields
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(
+        default=lambda self: fields.Date.today() + relativedelta(months=3)
+    )
+    expected_price = fields.Float()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ]
+    )
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        required=True,
+        copy=False,
+        default="new",
+    )
+    active = fields.Boolean(default=True)
+    estate_type_id = fields.Many2one("estate.type", string="Estate Type")
+    seller_id = fields.Many2one("res.users", string="Seller", default=lambda self: self.env.user)
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    tag_ids = fields.Many2many("estate.tag", string="Tags")
+    offer_ids = fields.One2many("estate.offer", "property_id", string="Offers")
