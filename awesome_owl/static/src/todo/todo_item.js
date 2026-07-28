eaa35ef8402e12f275cb3ca77030d9de1db3fc95
Tom Hermann (toher) <toher@odoo.com>
2026-07-28 14:51:52 +0200

    [ADD] Owl Chapt-1 7

---
+++ awesome_owl/static/src/todo/todo_item.js
@@ -0,0 +1,12 @@
+import { Component, useState } from "@odoo/owl";
+
+export class TodoItem extends Component {
+    static template = "awesome_owl.TodoItem";
+    static props = {
+        todo: {
+            id: Number,
+            decription: String,
+            isCompleted: Boolean,
+        }
+    }
+}
