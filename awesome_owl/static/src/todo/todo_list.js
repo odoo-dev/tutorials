d539ab73d4fc61ee21e8eff7dc592a3266e3ab78
Tom Hermann (toher) <toher@odoo.com>
2026-07-28 15:29:11 +0200

    [ADD] Owl Chapt-1 9

--- awesome_owl/static/src/todo/todo_list.js
+++ awesome_owl/static/src/todo/todo_list.js
@@ -6,11 +6,23 @@ export class TodoList extends Component {
     static components = { TodoItem };
 
     setup(){
+        this.currentId = 3
         this.todos = useState([
             { id: 3, description: "buy milk", isCompleted: false },
             { id: 2, description: "buy egg", isCompleted: true },
             { id: 1, description: "buy water", isCompleted: false }
         ]);
+        // this.todos = useState([])
     }
 
+    addTodo(ev) {
+        if (ev.keyCode === 13 && ev.target.value != "") {
+            this.todos.push({
+                id: ++this.currentId,
+                description: ev.target.value,
+                isCompleted: false
+            });
+            ev.target.value = "";
+        }
+    }
 }
