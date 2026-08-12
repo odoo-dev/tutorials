cfe5647cdd03a846120ccc4f9dd69bb4046427b2
Rohit Agarkar <rohitagarkar2004@gmail.com>
2026-08-12 17:38:40 +0530

    [ADD] estate: create real estate module structure
    - Add the Real Estate Odoo module manifest.
    - Add module and models initialization files.
    - Add the estate.property model file.
    - Configure the module to depend on the base module.
    - Mark the Real Estate module as installable and an application.

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Real Estate',
+    'category': 'Tutorials',
+    'summary': 'Real Estate App',
+    'installable': True,
+    'application': True,
+    'depends': [
+        'base',
+    ],
+}
