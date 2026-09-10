184d860b7be16d3c00665d10e0d371b64ec673f0
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2026-09-10 10:34:54 +0530

    [FIX] estate: fix property_types name using _rec_name attribute

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -4,5 +4,6 @@ from odoo import fields, models
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
     _description = 'Estate Property Type'
+    _rec_name = 'property_name'
 
     property_name = fields.Char(required=True)
