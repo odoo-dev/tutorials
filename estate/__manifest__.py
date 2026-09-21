2579924c296a0c64d0c6a4e425afbdb095ac298a
Surap <surap@odoo.com>
2026-09-21 14:24:28 +0200

    12.2-12.3 	modified:   estate/__manifest__.py 	modified:   estate/models/__init__.py 	new file:   estate/models/res_users.py 	new file:   estate/views/res_users_views.xml

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,7 @@
     'installable': True,
     'data': [
         'security/ir.model.access.csv',
+        'views/res_users_views.xml',
         'views/estate_property_views.xml',
         'views/estate_property_offer_views.xml',
         'views/estate_property_type_views.xml',
