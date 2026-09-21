b8e9a0d3737c9f7cf85cd212cc637b0a33ed69d5
Surap <surap@odoo.com>
2026-09-21 17:15:38 +0200

    13.3 (finally) 	modified:   estate_account/models/estate_property.py

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -37,23 +37,23 @@ class EstateProperty(models.Model):
             invoice_dict['partner_id'] = offer.partner_id.id
             invoice_dict['move_type'] = 'out_invoice'
             invoice_dict['journal_id'] = self.env['account.journal'].search([ ('type', '=', 'sale'), ('company_id', '=', self.env.company.id)], limit=1).id
-            # invoice_dict['invoice_line_ids'] = [
-            #     Command.create({
-            #         'name': 'Property price',
-            #         'quantity': 1,
-            #         'price_unit': offer.price,
-            #     }),
-            #     Command.create({
-            #         'name': 'Taxes',
-            #         'quantity': 1,
-            #         'price_unit': 0.06 * offer.price,
-            #     }),
-            #     Command.create({
-            #         'name': 'Administrative fees',
-            #         'quantity': 1,
-            #         'price_unit': 100000,
-            #     }),
-            # ]
+            invoice_dict['invoice_line_ids'] = [
+                Command.create({
+                    'name': 'Property price',
+                    'quantity': 1,
+                    'price_unit': offer.price,
+                }),
+                Command.create({
+                    'name': 'Taxes',
+                    'quantity': 1,
+                    'price_unit': 0.06 * offer.price,
+                }),
+                Command.create({
+                    'name': 'Administrative fees',
+                    'quantity': 1,
+                    'price_unit': 100000,
+                }),
+            ]
             to_invoice.append(invoice_dict)
         # debug
         moves = self.env['account.move'].sudo().with_context(default_move_type='out_invoice').create(to_invoice)
