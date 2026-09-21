e5380d464c06c07e203f8fb6ac84dcaf77143ec1
mrgor-odoo <mrgor@odoo.com>
2026-09-21 15:23:12 +0530

    [IMP] estate: allow manual ordering of property types
    Add sequence-based ordering so property types can be manually
    arranged in the list view.

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -4,13 +4,14 @@ from odoo import models, fields
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
     _description = "Property Type"
-    _order = 'name'
+    _order = 'sequence, name'
 
     name = fields.Char(required=True)
     property_ids = fields.One2many(
         'estate.property',
         'property_type_id'
     )
+    sequence = fields.Integer()
 
     _name_unique = models.Constraint(
         'UNIQUE(name)',
