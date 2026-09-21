324448a175cbad5bce755a153be6cd55dee60704
Olivier Le Thanh Duong <ollet@odoo.com>
2026-09-21 14:20:59 +0200

    [IMP] awesome_owl: implements section 2 to 5

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,12 +1,24 @@
 import {Counter} from "./counter/counter"
-import {Component} from "@odoo/owl";
+import {Card} from "./card/card"
+import {Component, markup, useState} from "@odoo/owl";
 
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
 
-    static components = {Counter};
+    static components = {Counter, Card};
+    title = "<i>Card 1</i>"
+    card_content = markup('<a href="http://">link</a>')
+
+    incrementSum() {
+        console.log('incrementSum');
+        this.state.total++;
+    }
 
     setup() {
+        console.log('setup');
+        this.state = useState({
+            'total': 0
+        });
     }
 }
