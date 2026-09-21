0f313450c6e53b6005f3cd85513569057e6d4a83
JeromeVerkyndt <jerome.verkyndt@gmail.com>
2026-09-21 10:50:26 +0200

    [IMP] estate: add list of properties in type property.

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -6,6 +6,7 @@ class PropertyType(models.Model):
     _description = "Estate property type"
 
     name = fields.Char(required=True)
+    property_ids = fields.One2many("estate.property", "property_type_id", "Property")
 
     _uniq_name = models.Constraint(
         'UNIQUE(name)',
