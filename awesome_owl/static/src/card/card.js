63b2a41f41cc867d0bfc8066621935b355fed6f2
Surap <surap@odoo.com>
2026-09-22 17:06:53 +0200

    [IMP]-Owl-1.5 	modified:   awesome_owl/static/src/card/card.js

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -2,7 +2,10 @@ import { Component, useState, xml } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card" ;
-
+    static props = {
+        title : {type : String},
+        content : {type : String},
+    }
     setup() {
         this.title = this.props.title  
         this.content = this.props.content  
