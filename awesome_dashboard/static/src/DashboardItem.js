81bf8483f5a4d386fd89892ee0d2d55e984fcac6
Olivier Le Thanh Duong <ollet@odoo.com>
2026-09-22 09:26:48 +0200

    [IMP] awesome dashboard: implement section 1 to 5

---
+++ awesome_dashboard/static/src/DashboardItem.js
@@ -0,0 +1,25 @@
+import {Component, useState, xml} from "@odoo/owl";
+
+export class DashboardItem extends Component {
+    static template = xml`
+        <div class="card d-inline-block m-2" t-attf-style="width: {{18* this.props.size}}rem">
+        <div class="card-body">
+        <h5 class="card-title"><t t-slot="title" /></h5>
+           <p class="card-text">
+            <t t-slot="default"/>
+            <div style="text-align: center;">
+                    <t t-esc="props.value" />            
+            </div>
+            </p>
+        </div>
+        </div>    `
+    static props = {
+        size: {type: Number, optional: true},
+        value: {type: Number, optional: true},
+        slots: {optional: true},
+    }
+    static defaultProps = {
+        size: 1,
+    };
+
+}
--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,8 +1,57 @@
-import { Component } from "@odoo/owl";
-import { registry } from "@web/core/registry";
+import {Component, onWillStart, useState} from "@odoo/owl";
+import {_t} from "@web/core/l10n/translation";
+
+import {registry} from "@web/core/registry";
+import {Layout} from "@web/search/layout";
+import {useService} from "@web/core/utils/hooks";
+import {DashboardItem} from "./DashboardItem";
+import {rpc} from "@web/core/network/rpc";
+
+//     "result": {
+//         "average_quantity": 7,
+//         "average_time": 6,
+//         "nb_cancelled_orders": 6,
+//         "nb_new_orders": 174,
+//         "orders_by_size": {
+//             "m": 14,
+//             "s": 4,
+//             "xl": 118
+//         },
+//         "total_amount": 117
+//     }
+
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = {Layout, DashboardItem};
+
+
+    setup() {
+        this.action = useService("action");
+        this.state = useState({stats: null})
+
+        onWillStart(async () => {
+            const result = await rpc("/awesome_dashboard/statistics")
+            console.log(result)
+            this.state.stats = result;
+            console.log(this.state.stats)
+
+        })
+
+    }
+
+    openCustomer() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    async openLeads(activity) {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: _t('Leads'),
+            res_model: 'crm.lead',
+            views: [[false, 'list'], [false, 'form']],
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
