66eccfea8239841a5f575f776709e5dee016cf16
manab-odoo <manab@odoo.com>
2026-09-22 16:16:25 +0200

    [IMP] Estate: Fogotten part of CH11 Inline

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -9,10 +9,8 @@ class EstatePropertyType(models.Model):
     name = fields.Char('Nom', required=True)
     property_ids = fields.One2many('estate_property', 'property_type_id', "Property")
     sequence = fields.Integer(string="Sequence", default=1)
-    
     _check_unique_name = models.Constraint(
         "UNIQUE (name)",
         "The name must be unique",
     )
-
-   
+ 
