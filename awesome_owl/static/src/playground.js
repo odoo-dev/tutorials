036143b8963da602223aa082432769a18711d79d
cailleaux-maxime <macai@odoo.com>
2026-09-22 15:30:53 +0200

    [IMP] awesome_owl: add markup display in Card
    Allow the usage of markup for the Card2

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,11 @@
-import { Component } from "@odoo/owl";
+import {Component, markup} from "@odoo/owl";
+import { Card } from "./card/card"
+import { Counter } from "./counter/counter"
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Card , Counter };
+
+    card1Text = '<div class=\'text-primary\'>some content</div>'
+    card2Text = markup(this.card1Text)
 }
