81ad051c089f9366570916355377a466711dc11e
Surap <surap@odoo.com>
2026-09-22 17:00:34 +0200

    [IMP]-Owl-1.3 	modified:   awesome_owl/static/src/card/card.xml 	modified:   awesome_owl/static/src/playground.js 	modified:   awesome_owl/static/src/playground.xml

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,9 +1,14 @@
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static components = { Counter , Card};
+    setup() {
+    }
+    
+    escape_value = "<div>some html</div>";
+    unescape_value = markup("<div>some html</div>");
 }
 
