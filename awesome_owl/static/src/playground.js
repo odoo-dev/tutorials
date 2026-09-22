af13b943b6711d702736377c0c528b92943ed2f7
cailleaux-maxime <macai@odoo.com>
2026-09-22 16:56:43 +0200

    [IMP] awesome_owl: add simple todo list
    Create a todo list with 2 components :
    todo_list & todo_item

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,11 @@
 import {Component, markup, useState} from "@odoo/owl";
 import { Card } from "./card/card"
 import { Counter } from "./counter/counter"
+import { TodoList } from "./todo_list/todo_list"
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Card , Counter };
+    static components = { Card , Counter, TodoList };
 
     setup() {
         this.sumOfCounter = useState({value: 2})
---
+++ awesome_owl/static/src/todo_list/todo_list.js
@@ -0,0 +1,17 @@
+import {Component, useState} from "@odoo/owl"
+import { TodoItem } from "../todo_item/todo_item";
+
+export class TodoList extends Component {
+    static template = 'awesome_owl.todo_list';
+    static components = { TodoItem }
+
+    setup(){
+        this.todos = useState([
+            { id: 3, description: "buy milk", isCompleted: false },
+            { id: 4, description: "buy Silk", isCompleted: false },
+            { id: 5, description: "Sell PS5", isCompleted: false },
+            { id: 6, description: "Update computer", isCompleted: false },
+        ]);
+    }
+
+}
