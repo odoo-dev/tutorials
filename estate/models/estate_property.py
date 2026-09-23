efbfb26c03c1e8e823ddc30fd3a48a9c7fecc492
Thore Mueller <thore.mueller07@gmail.com>
2026-09-23 10:07:06 +0200

    [FIX] Update access rights [ADD] Property Views (list view, form view) [ADD] Module menu to navigate to views [ADD] Reserved fields and attributes for existing fields [ADD] Search fields, filters and group by

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,18 +1,37 @@
-from odoo import models, fields
+from odoo import fields, models
+
 
 class EstateProperty(models.Model):
-    _name = 'estate.property'
-    _description = 'Estate Property'
+    _name = "estate.property"
+    _description = "Estate Property"
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3)
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection([('north', 'North'), ('east', 'East'), ('south', 'South'), ('west', 'West')])
+    garden_orientation = fields.Selection(
+        [("north", "North"), ("east", "East"), ("south", "South"), ("west", "West")],
+        default="north"
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("received", "Offer Received"),
+            ("accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        default="new",
+        required=True,
+        copy=False,
+    )
