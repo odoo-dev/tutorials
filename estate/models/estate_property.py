b005e3701d2aefb172d3317f0105ed258a033307
lokesh <losbo@odoo.com>
2026-09-23 18:56:23 +0530

    [IMP] estate: implement property state actions and status workflow
    - Added buttons in property form view to accept, sell, or cancel offers.
    - Added action methods in Python to update property state dynamically.
    - Implemented raise UserError checks for invalid state transitions.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,8 +1,7 @@
 from dateutil.relativedelta import relativedelta
-from odoo import api
-from odoo import fields
-from odoo import models
-from odoo.exceptions import ValidationError
+
+from odoo import api, fields, models
+from odoo.exceptions import UserError, ValidationError
 
 
 class EstateProperty(models.Model):
@@ -31,26 +30,29 @@ class EstateProperty(models.Model):
             ("west", "West"),
         ],
         string="Direction",
-        default="north",
+        default="north"
     )
     state = fields.Selection(
         [
             ("new", "New"),
-            ("offer_received", "Offer Receeived"),
+            ("offer_received", "Offer Received"),
             ("offer_accepted", "Offer Accepted"),
             ("sold", "Sold"),
             ("cancelled", "Cancelled"),
-        ]
+        ],
+        default="new",
+        compute="_compute_state",
+        store=True
     )
     active = fields.Boolean("Active", default=True)
-    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    property_type_id = fields.Many2one(
+        "estate.property.type", string="Property Type")
     property_tag_ids = fields.Many2many(
         "estate.property.tag",
         relation="losbo_property_propertyTag",
-        string="Property_Tag",
         column1="estate_property_id",
         column2="estate_property_tag_id",
-        ondelete="cascade",
+        ondelete="cascade"
     )
     buyer = fields.Many2one("res.partner", string="buyer", copy=False)
     user_id = fields.Many2one(
@@ -70,7 +72,8 @@ class EstateProperty(models.Model):
     def _check_description(self):
         for record in self:
             if record.name == record.description:
-                raise ValidationError("Fields name and description should not be equal")
+                raise ValidationError(
+                    "Fields name and description should not be equal")
 
     @api.depends("living_area", "garden_area")
     def _compute_area(self):
@@ -80,6 +83,43 @@ class EstateProperty(models.Model):
     @api.depends("offer_ids.price")
     def _compute_price(self):
         for records in self:
-            records.best_price = max(records.offer_ids.mapped("price"), default=0)
+            records.best_price = max(
+                records.offer_ids.mapped("price"), default=0)
 
-    # @api.onchange('')
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        for records in self:
+            if records.garden:
+                records.garden_area = 10
+                records.garden_orientation = "north"
+            else:
+                records.garden_area = 0
+                records.garden_orientation = ""
+
+    @api.depends("offer_ids.status")
+    def _compute_state(self):
+        for record in self:
+            if record.state not in ("sold", "cancelled"):
+                if "accepted" in record.offer_ids.mapped("status"):
+                    record.state = "offer_accepted"
+                elif len(record.offer_ids) > 0:
+                    record.state = "offer_received"
+
+    def property_sold(self):
+        for record in self:
+            if record.state == "cancelled":
+                raise UserError("Cancelled property cannot be sold")
+            elif record.state == "sold":
+                raise UserError("Property is already sold")
+            if "accepted" in record.offer_ids.mapped("status"):
+                record.state = "sold"
+            else:
+                raise UserError("There should be one accepted offer")
+
+    def property_cancel(self):
+        for record in self:
+            if record.state == "sold":
+                raise UserError("Sold property cannot be cancelled")
+            elif record.state == "cancelled":
+                raise UserError("Property is already cancelled")
+            record.state = "cancelled"
--- estate/models/estate_property_offers.py
+++ estate/models/estate_property_offers.py
@@ -1,8 +1,7 @@
 from datetime import date
 from dateutil.relativedelta import relativedelta
-from odoo import api
-from odoo import fields
-from odoo import models
+
+from odoo import api, fields, models
 
 
 class PropertyOffer(models.Model):
@@ -10,33 +9,51 @@ class PropertyOffer(models.Model):
     _description = "this model is used to define the offers received to the property"
 
     price = fields.Float()
-    status = fields.Selection([("accepted", "Accepted"), ("refused", "Refused")])
+    status = fields.Selection(
+        [("accepted", "Accepted"), ("refused", "Refused"), ("pending", "Pending")],
+        default="pending",
+    )
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True, ondelete="cascade")
     deadline = fields.Date(
         default=date.today(),
         copy=False,
         compute="_compute_deadline",
-        readonly=False,
-        # store=True,
+        inverse="_inverse_deadline",
+        store=True,
     )
-    validity_days = fields.Integer(
-        default=0,
-        copy=False,
-        compute="_compute_validity",
-        readonly=False,
-        # store=True
-    )
+    validity_days = fields.Integer(default=7, copy=False)
 
     @api.depends("validity_days")
     def _compute_deadline(self):
         for records in self:
-            records.deadline = date.today() + relativedelta(days=records.validity_days)
+            base_date = (
+                records.create_date.date()
+                if records.create_date
+                else fields.Date.today()
+            )
+            records.deadline = base_date + relativedelta(days=records.validity_days)
 
-    @api.depends("deadline")
-    def _compute_validity(self):
+    def _inverse_deadline(self):
         for records in self:
-            diff = relativedelta(records.deadline, date.today())
+            base_date = (
+                records.create_date.date()
+                if records.create_date
+                else fields.Date.today()
+            )
+            diff = relativedelta(records.deadline, base_date)
             records.validity_days = diff.days
 
-    # def inverse_func(self):
+    def action_confirm(self):
+        self.status = "accepted"
+        self.property_id.buyer = self.partner_id
+        self.property_id.selling_price = self.price
+        offers = self.env["estate.property.offers"].browse(
+            self.env["estate.property.offers"].search([]).ids
+        )
+        for record in offers:
+            if record.id != self.id:
+                record.status = "refused"
+
+    def action_cancel(self):
+        self.status = "refused"
