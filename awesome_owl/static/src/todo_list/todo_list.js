4365336f5fd9544ff2bad313d1343a330ae51565
Surap <surap@odoo.com>
2026-09-23 11:16:33 +0200

    [ADD]-todolists -> change style of done todos -Owl-1.8 	modified:   awesome_owl/static/src/todo_list/todo_item.xml 	modified:   awesome_owl/static/src/todo_list/todo_list.js

--- awesome_owl/static/src/todo_list/todo_list.js
+++ awesome_owl/static/src/todo_list/todo_list.js
@@ -19,7 +19,9 @@ export class TodoList extends Component {
     // }
     setup() {
         this.todos = useState([
-            { id: 3, description: "buy milk", isCompleted: false },
+            { id: 1, description: "buy milk", isCompleted: true },
+            { id: 2, description: "buy milk", isCompleted: false },
+            { id: 3, description: "buy milk", isCompleted: true },
             { id: 4, description: "buy machin", isCompleted: false },
             { id: 5, description: "buy bidule", isCompleted: false }]);
         console.info(this);
