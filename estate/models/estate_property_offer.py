ce95789aa1f1a6305be0377bda6073a5291ed282
smitinit <patelsmit090305@gmail.com>
2026-09-25 11:22:47 +0530

    [ADD] estate: add offer validity and deadline
    Add validity and deadline fields to property offer
    with computed and inverse logic, and display them
    in the offer views.

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from datetime import timedelta
 
 
 class EstatePropertyOffer(models.Model):
@@ -13,3 +14,23 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer("Validity (days)", default=7)
+    date_deadline = fields.Date(
+        "Deadline",
+        compute="_compute_date_deadline",
+        inverse="_inverse_date_deadline",
+    )
+
+    @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = (
+                record.create_date or fields.Datetime.now()
+            ) + timedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.create_date and record.date_deadline:
+                # using .date() on create_date because it has type of datetime
+                date_diff = record.date_deadline - record.create_date.date()
+                record.validity = date_diff.days
